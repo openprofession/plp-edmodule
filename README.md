387c4bd042f9bc2734a1d6796833108d16596f2c
nosanity <no-sanity17@ya.ru>
2016-07-27 15:29:47 +0300

    EM-1 Создать приложение для образовательных модулей как расширение plp

---
+++ README.md
@@ -0,0 +1,5 @@
+## Установка
+
+    Установить в plp:
+    pip install -e https://github.com/openprofession/plp-edmodule@master#egg=plp_edmodule
+    Запускать plp с настройками из файла openprof_example.py
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plp-edmodule',
+    version='0.1',
+    packages=find_packages(),
+    description='PLP edmodule',
+    url='http://example.com',
+    author='author',
+)
