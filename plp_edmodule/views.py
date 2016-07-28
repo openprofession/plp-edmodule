ee123060409d17c25be20c4879e93a81d8997ba3
nosanity <no-sanity17@ya.ru>
2016-07-28 18:38:02 +0300

    EM-8 Update записи на модуль

--- plp_edmodule/views.py
+++ plp_edmodule/views.py
@@ -20,25 +20,42 @@ if RAVEN_CONFIG:
 @require_POST
 def edmodule_enroll(request):
     ed_module_code = request.POST.get('ed_module_code', '')
+    try:
+        is_active = request.POST['is_active'] in ['true', 'True', True]
+    except KeyError:
+        is_active = True
     bad_ed_module_code = False
     if ed_module_code:
         try:
             edmodule = EducationalModule.objects.get(code=ed_module_code)
             try:
                 enrollment = EducationalModuleEnrollment.objects.get(user=request.user, module=edmodule)
-                if enrollment.is_active:
+                if enrollment.is_active and is_active:
                     logging.info('User {} already enrolled in educational module {}'.format(
                         request.user.username, edmodule.code
                     ))
                     return JsonResponse({'status': 1})
-                enrollment.is_active = True
+                elif not enrollment.is_active and not is_active:
+                    logging.info('User {} already unenrolled from educational module {}'.format(
+                        request.user.username, edmodule.code
+                    ))
+                    return JsonResponse({'status': 1})
+                enrollment.is_active = is_active
                 enrollment.save()
             except EducationalModuleEnrollment.DoesNotExist:
+                if not is_active:
+                    if client:
+                        client.captureMessage('User without active enrollment tried to unenroll from module',
+                                              extra={'user': request.user.username, 'module': edmodule.code})
+                    logging.error('User {} without active enrollment tried to unenroll from module {}'.format(
+                        request.user.username, edmodule.code
+                    ))
+                    return JsonResponse({'status': 1})
                 EducationalModuleEnrollment.objects.create(
-                    user=request.user, module=edmodule, is_active=True
+                    user=request.user, module=edmodule, is_active=is_active
                 )
-            logging.info('User {} successfully enrolled in educational module {}'.format(
-                request.user.username, edmodule.code
+            logging.info('User {} successfully {} educational module {}'.format(
+                request.user.username, 'enrolled in' if is_active else 'unenrolled from', edmodule.code
             ))
             return JsonResponse({'status': 0})
         except EducationalModule.DoesNotExist:
