de96b44225c1ca2a4d337f7e002dec46de9dcf82
nosanity <no-sanity17@ya.ru>
2016-09-06 13:38:39 +0300

    проект может принадлежать только одному модулю

--- plp_edmodule/admin.py
+++ plp_edmodule/admin.py
@@ -1,12 +1,33 @@
 # coding: utf-8
 
 from django.contrib import admin
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 from autocomplete_light import modelform_factory
+from plp_extension.apps.course_extension.models import CourseExtendedParameters
 from .models import EducationalModule, EducationalModuleEnrollment
 
 
+class EducationalModuleAdminForm(forms.ModelForm):
+    class Meta:
+        model = EducationalModule
+        fields = '__all__'
+
+    def clean_courses(self):
+        courses = self.cleaned_data.get('courses')
+        if courses:
+            proj = CourseExtendedParameters.objects.filter(course__id__in=[i.id for i in courses], is_project=True)
+            if proj:
+                for p in proj:
+                    if EducationalModule.objects.filter(courses=p.course).count() > 0:
+                        raise forms.ValidationError(_(u'Проект {title} уже содержится в другом модуле').format(
+                            title=p.course.title
+                        ))
+        return courses
+
+
 class EducationalModuleAdmin(admin.ModelAdmin):
-    pass
+    form = EducationalModuleAdminForm
 
 
 class EducationalModuleEnrollmentAdmin(admin.ModelAdmin):
